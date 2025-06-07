9769206e0262be297f1b7a4ae1c4c26309697b8c
Anil kumar padigela <123974852+pnlkmr43@users.noreply.github.com>
2025-06-07 16:00:52 +0530

    Update top_uart.v

--- top_uart.v
+++ top_uart.v
@@ -18,13 +18,17 @@ module top_uart_send_once(
         .busy(busy)
     );
 
+
     // Generate single send pulse after some delay (simulate button press)
     always @(posedge clk) begin
-        if (counter == 100_000_000) begin // after 1 sec
+        counter <= counter + 1;
+        
+        if (counter == 100000000) begin // after 1 sec
+            counter <= 0;
             trigger <= 1;
         end else begin
             trigger <= 0;
         end
-        counter <= counter + 1;
+        
     end
 endmodule
